9272f49947fea9ec68ee7448e2836e7cebbb16e8
Rodrigo Sandoval <sandovalrgg@gmail.com>
2021-12-10 22:36:38 -0600

    feat(users): Creación de módulo de usuarios.

---
+++ src/user/user.service.ts
@@ -0,0 +1,42 @@
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
+import { Model } from 'mongoose';
+import { USER } from 'src/common/models/models';
+import { IUser } from '../common/interfaces/user.interface';
+import { UserDTO } from './dto/user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectModel(USER.name) private readonly model: Model<IUser>) {}
+
+  async hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+  }
+
+  async create(userDTO: UserDTO): Promise<IUser> {
+    const hash = await this.hashPassword(userDTO.password);
+    const newUser = new this.model({ ...userDTO, password: hash });
+    return await newUser.save();
+  }
+
+  async findAll(): Promise<IUser[]> {
+    return await this.model.find();
+  }
+
+  async findOne(id: string): Promise<IUser> {
+    return await this.model.findById(id);
+  }
+
+  async update(id: string, userDTO: UserDTO): Promise<IUser> {
+    const hash = await this.hashPassword(userDTO.password);
+    const user = { ...userDTO, password: hash };
+    return await this.model.findByIdAndUpdate(id, user, { new: true });
+  }
+
+  async delete(id: string) {
+    await this.model.findByIdAndDelete(id);
+    return { status: HttpStatus.OK, msg: 'Deleted' };
+  }
+}
